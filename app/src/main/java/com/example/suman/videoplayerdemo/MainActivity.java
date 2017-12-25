84e5e9e73b86db416ed5899cd90a09ea609aa61f
Suman Neupane <sumanmyon.sam@gmail.com>
2017-12-25 20:38:32 +0545

    fine

--- app/src/main/java/com/example/suman/videoplayerdemo/MainActivity.java
+++ app/src/main/java/com/example/suman/videoplayerdemo/MainActivity.java
@@ -1,17 +1,13 @@
 package com.example.suman.videoplayerdemo;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.database.Cursor;
 import android.media.MediaMetadataRetriever;
 
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -26,29 +22,12 @@ public class MainActivity extends Activity
     private MediaController mController;
 
     MediaMetadataRetriever mMetadataRetriever;
-    private static final int REQUEST_READ_PHONE_STATE = 110, REQUEST_WRITE_STORAGE = 112;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        boolean hasPermissionPhoneState = (ContextCompat.checkSelfPermission(getApplicationContext(),
-                Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED);
-        if (!hasPermissionPhoneState) {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.READ_PHONE_STATE},
-                    REQUEST_READ_PHONE_STATE);
-        }
-
-        boolean hasPermissionWrite = (ContextCompat.checkSelfPermission(getApplicationContext(),
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
-        if (!hasPermissionWrite) {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    REQUEST_WRITE_STORAGE);
-        }
 
         mVideoView = (VideoView) findViewById(R.id.videoView);
 
@@ -60,18 +39,6 @@ public class MainActivity extends Activity
         intent.setAction(Intent.ACTION_GET_CONTENT);
 
         startActivityForResult(Intent.createChooser(intent, "Video File to Play"), 0);
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig)
-    {
-        super.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-
-        return super.onKeyDown(keyCode, event);
     }
 
     public void startPlayback(String videoPath)
@@ -86,14 +53,7 @@ public class MainActivity extends Activity
         //mVideoView.requestFocus();
         mVideoView.start();
     }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu)
-//    {
-//        getMenuInflater().inflate(R.menu.video_view_sample, menu);
-//
-//        return true;
-//    }
+    
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
